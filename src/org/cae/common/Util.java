af6f62d71cf6ad604337d37e52de16233ea44e51
liuyang <liuyang@Lenovo-PC>
2017-05-15 13:34:16 +0800

    新增压缩文件的方法

--- src/org/cae/common/Util.java
+++ src/org/cae/common/Util.java
@@ -1,7 +1,9 @@
 package org.cae.common;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
@@ -12,6 +14,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.log4j.Logger;
 
@@ -23,7 +26,7 @@ public class Util {
 	private static SimpleDateFormat timeSdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	public static final String destPath="D:\\nginx-1.12.0\\html\\aqours";
 	public static final Charset charset=Charset.forName("gbk");
-	
+    public static final byte[] buf = new byte[1024];
 	public static String toJson(Object target){
 		ObjectMapper mapper=new ObjectMapper();
 		try{
@@ -99,7 +102,11 @@ public class Util {
 		}
 		logger.error(stackInfo);
 	}
-	
+/**
+ * 			解压文件
+ * @param in文件输入流
+ * @return	解压的文件名集合
+ */
 	public  static List<String> unZip(InputStream in){
 
 		ZipInputStream zipInputStream = new ZipInputStream(in,charset);
@@ -116,10 +123,9 @@ public class Util {
             }
 			songName.add(fileName.substring(0, fileName.lastIndexOf(".")));
             OutputStream out = new FileOutputStream(filePath);
-            byte[] buf1 = new byte[1024];
             int len;
-            while((len=zipInputStream.read(buf1))>0){
-                out.write(buf1,0,len);
+            while((len=zipInputStream.read(buf))>0){
+                out.write(buf,0,len);
             }
             
             out.close();
@@ -131,4 +137,57 @@ public class Util {
 		return new ArrayList<>();
 	}
 	}
+/**
+ * 		压缩文件
+ * @param zip 生成的Zip文件名
+ * @param base	压入Zip的目录，默认为根目录
+ * @param filePath 	压缩的文件路径
+ * @throws IOException
+ */
+	public static void ZipFiles(String zip,String base,String filePath) throws IOException{ 
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zip)); 
+        File[] srcFiles=new File(filePath).listFiles();
+        ZipFiles(out,base,srcFiles); 
+        out.close(); 
+    } 
+/**
+ *  压缩文件(原始方法)
+ * @param out	压缩输出流
+ * @param base	压入Zip的目录，默认为根目录
+ * @param srcFiles	压缩的文件路径
+ */
+	public static void ZipFiles(ZipOutputStream out,String base,File[] srcFiles){ 
+        base = base.replaceAll("\\*", "/"); 
+        if(!base.endsWith("/")){ 
+            base+="/"; 
+        } 
+        byte[] buf = new byte[1024]; 
+        try { 
+            for(int i=0;i<srcFiles.length;i++){ 
+                if(srcFiles[i].isDirectory()){ 
+                    File[] files = srcFiles[i].listFiles(); 
+                    String srcPath = srcFiles[i].getName(); 
+                    srcPath = srcPath.replaceAll("\\*", "/"); 
+                    if(!srcPath.endsWith("/")){ 
+                        srcPath+="/"; 
+                    } 
+                    out.putNextEntry(new ZipEntry(base+srcPath)); 
+                    ZipFiles(out,base+srcPath,files); 
+                } 
+                else{ 
+                    FileInputStream in = new FileInputStream(srcFiles[i]); 
+                    out.putNextEntry(new ZipEntry(base + srcFiles[i].getName())); 
+                    int len; 
+                    while((len=in.read(buf))>0){ 
+                        out.write(buf,0,len); 
+                    } 
+                    out.closeEntry(); 
+                    in.close(); 
+                } 
+            } 
+        } catch (Exception e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+
 }
